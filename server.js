6dc642414b8492b89165ba28a1deeddceadc0d1a
Taylor Welling <tw3080@gmail.com>
2016-10-03 13:02:16 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+
+// Calls the Spotify Web API
+var getFromApi = function(endpoint, args) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/' + endpoint)
+        .qs(args)
+        .end(function(response) {
+            if (response.ok) {
+                emitter.emit('end', response.body);
+            } else {
+                emitter.emit('error', response.code);
+            }
+        });
+    return emitter;
+};
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/search/:name', function(req, res) {
+    var searchReq = getFromApi('search', {
+        q: req.params.name,
+        limit: 1,
+        type: 'artist'
+    });
+
+    // Gets an artist by name
+    searchReq.on('end', function(item) {
+        var artist = item.artists.items[0];
+        var relatedArtist = artist.id + '/related-artists';
+        var relatedReq = getFromApi('artists/' + relatedArtist);
+
+        // Gets related artists
+        relatedReq.on('end', function(item) {
+            artist.related = item.artists;
+            res.json(artist);
+        });
+
+        relatedReq.on('error', function(code) {
+            res.sendStatus(code);
+        });
+    });
+
+    searchReq.on('error', function(code) {
+        res.sendStatus(code);
+    });
+});
+
+app.listen(process.env.PORT || 8080);
